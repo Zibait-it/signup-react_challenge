2f6268c3f0f37e2f66ad995f551eb1f452cc47af
Zibait-it <lucas.zibaitis@lightit.io>
2024-10-09 12:03:14 -0300

    feat: create loading and success screen

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,14 +1,19 @@
 interface ButtonProps {
   text: string;
   variant: "primary" | "secondary";
+  onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-export default function Button({ text, variant }: ButtonProps) {
+export default function Button({ text, variant, onClick }: ButtonProps) {
   const variantStyles = {
     primary:
       "bg-complementary-500 text-primary-50 text-base font-manrope font-semibold flex items-center justify-center rounded-full py-3.5 lg:w-full",
     secondary: "",
   };
 
-  return <button className={variantStyles[variant]}>{text}</button>;
+  return (
+    <button className={variantStyles[variant]} onClick={onClick}>
+      {text}
+    </button>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,8 @@ import Button from "./components/Button";
 import Letter from "./components/Icons/Letter";
 import Lock from "./components/Icons/Lock";
 import User from "./components/Icons/User";
+import LoadingSpinner from "./components/Icons/LoadingSpinner";
+import Confetti from "./components/Icons/Confetti";
 
 export default function SignUp() {
   const [formData, setFormData] = useState({
@@ -14,6 +16,8 @@ export default function SignUp() {
     password: "",
     confirmPassword: "",
   });
+  const [loading, setLoading] = useState(false);
+  const [completed, setCompleted] = useState(false);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
     const { name, value } = e.target;
@@ -23,66 +27,98 @@ export default function SignUp() {
     }));
   };
 
+  const handleSubmit = (): void => {
+    setLoading(true);
+    setTimeout(() => {
+      setCompleted(true);
+      setLoading(false);
+    }, 1000);
+    setCompleted(true);
+  };
+
   return (
-    <main className="flex flex-col lg:flex-row gap-16 bg-background h-full lg:h-screen pt-16 pb-24 px-6 lg:gap-0 lg:pt-0 lg:pb-0 lg:px-0">
+    <main className="flex flex-col lg:flex-row gap-16 bg-background min-h-screen h-screen pt-16 pb-24 px-6 lg:gap-0 lg:pt-0 lg:pb-0 lg:px-0">
       <div className="w-full lg:h-full lg:w-3/5 lg:bg-[url('/images/Image1.jpg')] lg:bg-cover lg:bg-no-repeat lg:bg-top lg:pl-10 lg:pt-8">
         <Image src="/images/logo.svg" width={195} height={33} alt="Logo" />
       </div>
-      <div className="flex flex-col gap-14 w-full lg:w-2/5 lg:justify-center lg:items-center lg:px-16">
-        <div className="flex flex-col gap-6 lg:w-full">
-          <h1 className="font-ubuntu text-3xl font-medium text-primary-700">
-            Sign up
-          </h1>
-          <div className="flex flex-col gap-1 font-manrope font-normal text-base text-primary-600 tracking-wide">
-            <p>If you already have an account register</p>
-            <p>
-              You can{" "}
-              <a className="text-complementary-500 font-bold underline" href="">
-                Login here!
-              </a>
-            </p>
-          </div>
-        </div>
-
-        <form className="flex flex-col gap-8 lg:w-full">
-          <Input
-            icon={<Letter />}
-            label="Email"
-            placeholder="Enter your email address"
-            type="text"
-            name="email"
-            onChange={handleChange}
-            value={formData.email}
-          />
-          <Input
-            icon={<User />}
-            label="Username"
-            placeholder="Enter your username"
-            type="text"
-            name="username"
-            onChange={handleChange}
-            value={formData.username}
-          />
-          <Input
-            icon={<Lock />}
-            label="Password"
-            placeholder="Enter your password"
-            type="password"
-            name="password"
-            onChange={handleChange}
-            value={formData.password}
-          />
-          <Input
-            icon={<Lock />}
-            label="Confirm Password"
-            placeholder="Confirm your password"
-            type="password"
-            name="confirmPassword"
-            onChange={handleChange}
-            value={formData.confirmPassword}
-          />
-        </form>
-        <Button text="Register" variant="primary" />
+      <div className="w-full h-full lg:w-2/5 lg:px-16 ">
+        {loading ? (
+          <section className="w-full h-full flex items-center justify-center">
+            <LoadingSpinner />
+          </section>
+        ) : completed ? (
+          <section
+            className={`w-full h-full flex flex-col items-center justify-center text-center gap-6`}
+          >
+            <Confetti />
+            <h2
+              className={`font-ubuntu font-medium text-4xl text-complementary-700`}
+            >
+              Great!
+              <br />
+              You signed in
+            </h2>
+          </section>
+        ) : (
+          <section className="w-full h-full flex flex-col gap-14 lg:justify-center lg:items-center">
+            <div className="flex flex-col gap-6 lg:w-full">
+              <h1 className="font-ubuntu text-3xl font-medium text-primary-700">
+                Sign up
+              </h1>
+              <div className="flex flex-col gap-1 font-manrope font-normal text-base text-primary-600 tracking-wide">
+                <p>If you already have an account register</p>
+                <p>
+                  You can{" "}
+                  <a
+                    className="text-complementary-500 font-bold underline"
+                    href=""
+                  >
+                    Login here!
+                  </a>
+                </p>
+              </div>
+            </div>
+            <form className="flex flex-col gap-8 lg:w-full">
+              <Input
+                icon={<Letter />}
+                label="Email"
+                placeholder="Enter your email address"
+                type="text"
+                name="email"
+                onChange={handleChange}
+                value={formData.email}
+              />
+              <Input
+                icon={<User />}
+                label="Username"
+                placeholder="Enter your username"
+                type="text"
+                name="username"
+                onChange={handleChange}
+                value={formData.username}
+              />
+              <Input
+                icon={<Lock />}
+                label="Password"
+                placeholder="Enter your password"
+                type="password"
+                name="password"
+                onChange={handleChange}
+                value={formData.password}
+              />
+              <Input
+                icon={<Lock />}
+                label="Confirm Password"
+                placeholder="Confirm your password"
+                type="password"
+                name="confirmPassword"
+                onChange={handleChange}
+                value={formData.confirmPassword}
+              />
+            </form>
+            <Button text="Register" variant="primary" onClick={handleSubmit} />
+          </section>
+        )}
       </div>
     </main>
   );
